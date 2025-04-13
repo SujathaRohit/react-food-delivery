4c443e3e74779e0e4560836693d401ec9590adc2
SujathaRohit <sujathatukula@gmail.com>
2025-04-12 21:49:53 -0700

    Create Menu items

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Navbar from "./components/Navbar/Navbar";
+import {Routes, Route} from "react-router-dom";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
 
 const App = ()=>{
   return(
     <div className="app">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import ExploreMenu from '../../components/ExpoloreMenu/ExpoloreMenu';
+import Header from '../../components/Header/Header';
+import "./Home.css";
+
+const Home = () =>{
+    return(
+        <div>
+            <Header />
+            <ExploreMenu/>
+        </div>
+    )
+}
+export default Home;
