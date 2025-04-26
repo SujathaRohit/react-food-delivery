13607e7dc9b3697b2f9d3039bee69ea0f3edf455
SujathaRohit <sujathatukula@gmail.com>
2025-04-25 18:11:15 -0700

    adding items under Top dishes near you and adding + and - icons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import ExploreMenu from '../../components/ExpoloreMenu/ExpoloreMenu';
 import Header from '../../components/Header/Header';
 import "./Home.css";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 
-const Home = () =>{
-    return(
+const Home = () => {
+
+    const [category, setCategory] = useState("All");
+    return (
         <div>
             <Header />
-            <ExploreMenu/>
+            <ExploreMenu category={category} setCategory={setCategory} />
+            <FoodDisplay category={category}/>
         </div>
     )
 }
